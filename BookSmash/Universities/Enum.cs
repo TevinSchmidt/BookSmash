1eafbaf6f094e96105db2cb70bbeedc3c7202860
James Bews <jbews11@gmail.com>
2018-03-19 16:17:29 -0600

    Fe

---
+++ BookSmash/Universities/Enum.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookSmash.Universities
+{
+   public enum University { "University of Calgary", "Univeristy of Alberta", "University of Leth" };
+}
